b85762d25a693403b62380508302af6ad9b1ab54
Sezayir Dağtekin <sezayir@hotmail.com>
2021-02-01 16:11:19 +0300

    App config

--- mongodb/src/main/java/com/sezayir/mongodb/configuration/AppConfig.java
+++ mongodb/src/main/java/com/sezayir/mongodb/configuration/AppConfig.java
@@ -3,11 +3,8 @@ package com.sezayir.mongodb.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
